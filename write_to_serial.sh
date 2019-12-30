4bf2dbe01ff7942fbb20650bec75a50d2f17e1df
Robin SHAMSNEJAD <rsham@free.fr>
2019-12-30 17:33:06 +0100

    Added complete argument checking and improved sanity checks

--- write_to_serial.sh
+++ write_to_serial.sh
@@ -4,11 +4,60 @@
 # OPTION PARSING
 ################################################################################
 
-USAGE=$(cat <<- EOT
-	Usage : $0 <serial device>
-	Example : $0 /dev/ttyUSB0
-EOT
+### BEGIN GETOPT
+
+OPTS=`getopt -o hblc --long help,block,line,character: -n 'parse-options' -- "$@"`
+
+if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
+
+# echo "$OPTS"
+eval set -- "$OPTS"
+
+HELP=false
+BLOCK=false
+LINE=false
+CHARACTER=false
+
+while true; do
+	case "$1" in
+		-h | --help )		HELP=true; shift ;;
+		-b | --block )		BLOCK=true; shift ;;
+		-l | --line )		LINE=true; shift ;;
+		-c | --character )	CHARACTER=true; shift ;;
+		-- ) shift; break ;;
+		* ) break ;;
+  	esac
+done
+
+# echo HELP=$HELP
+# echo BLOCK=$BLOCK
+# echo LINE=$LINE
+# echo CHARACTER=$CHARACTER
+
+### END GETOPT
+
+### BEGIN SANITY CHECK
+
+USAGE=$(cat << EOF
+Usage : $0 [-hblc] <serial device>
+Example : $0 -l /dev/ttyUSB0
+
+-h, --help : 	  Show this message
+
+-b, --block : 	  Block mode. In this mode you can freely type,
+		  and all will be sent to the device after the
+		  following character sequence :
+			  EOT<Enter>
+
+-l, --line :	  Line mode. In this mode everything is sent
+		  upon pressing <Enter> (which is not sent itself).
+		  This is much like a terminal behavior.
+
+-c, --character : Character mode. In this mode, each single entered
+		  character is sent straight away on key press.
+EOF
 )
+
 DEVICE=$1
 
 if [ ! $DEVICE ] || [ $# != 1 ] ; then
@@ -31,9 +80,24 @@ elif ! [ -w "$DEVICE" ] ; then
 	exit 1
 fi
 
+if ( $BLOCK && $LINE ) || \
+( $BLOCK && $CHARACTER ) || \
+( $LINE && $CHARACTER ) || \
+( ! ( $BLOCK || $LINE || $CHARACTER ) )
+then
+	echo >&2 "ERROR : Please choose a single mode of operation."
+	echo
+	echo >&2 "$USAGE"
+	exit 1
+fi
+
+### END SANITY CHECK
+
 ################################################################################
 # BEGINNING OF ACTUAL PROGRAM
 ################################################################################
+
+
 
 cat << EOT
 ============== Serial port output ===============
