45aa1c5afa93a5b4f2b8fe0581420b04f3456cf0
Robin SHAMSNEJAD <rsham@free.fr>
2020-09-23 16:11:59 +0200

    First source code skeleton

---
+++ src/cpp/write_to_fd.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(/* argc + argv */)
+{
+	//parse_command_line();
+	//interpret_arguments();
+	//open_fd();
+	//get_input();
+	return EXIT_SUCCESS;
+}
